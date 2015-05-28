0fe8f18fe81e4f5f595ca9fa3cb62c6156453376
nikhilnz <nikhilsharma@hotmail.com>
2015-05-28 22:00:33 +0100

    use shared examples

--- spec/calendar_helper_spec.rb
+++ spec/calendar_helper_spec.rb
@@ -25,54 +25,61 @@ describe CalendarHelper::DateWrapper do
   end
 
   context '#last_day_of_month' do
-    it { expect(described_class.new('January', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('February', 2014).last_day_of_month).to eq(28) }
-    it { expect(described_class.new('March', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('April', 2014).last_day_of_month).to eq(30) }
-    it { expect(described_class.new('May', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('June', 2014).last_day_of_month).to eq(30) }
-    it { expect(described_class.new('July', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('August', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('September', 2014).last_day_of_month).to eq(30) }
-    it { expect(described_class.new('October', 2014).last_day_of_month).to eq(31) }
-    it { expect(described_class.new('November', 2014).last_day_of_month).to eq(30) }
-    it { expect(described_class.new('December', 2014).last_day_of_month).to eq(31) }
-
-    it { expect(described_class.new('February', 2012).last_day_of_month).to eq(29) }
+    shared_examples_for 'last day of month' do |month, year, last_day|
+      it { expect(described_class.new(month, year).last_day_of_month).to eq(last_day) }
+    end
+    it_should_behave_like 'last day of month', 'January', 2014, 31
+    it_should_behave_like 'last day of month', 'February', 2014, 28
+    it_should_behave_like 'last day of month', 'March', 2014, 31
+    it_should_behave_like 'last day of month', 'April', 2014, 30
+    it_should_behave_like 'last day of month', 'May', 2014, 31
+    it_should_behave_like 'last day of month', 'June', 2014, 30
+    it_should_behave_like 'last day of month', 'July', 2014, 31
+    it_should_behave_like 'last day of month', 'August', 2014, 31
+    it_should_behave_like 'last day of month', 'September', 2014, 30
+    it_should_behave_like 'last day of month', 'October', 2014, 31
+    it_should_behave_like 'last day of month', 'November', 2014, 30
+    it_should_behave_like 'last day of month', 'December', 2014, 31
+    it_should_behave_like 'last day of month', 'February', 2012, 29
 
   end
 
   context '#total_days' do
-    it { expect(described_class.new('January', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('February', 2014).total_days).to eq(28) }
-    it { expect(described_class.new('March', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('April', 2014).total_days).to eq(30) }
-    it { expect(described_class.new('May', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('June', 2014).total_days).to eq(30) }
-    it { expect(described_class.new('July', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('August', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('September', 2014).total_days).to eq(30) }
-    it { expect(described_class.new('October', 2014).total_days).to eq(31) }
-    it { expect(described_class.new('November', 2014).total_days).to eq(30) }
-    it { expect(described_class.new('December', 2014).total_days).to eq(31) }
-
-    it { expect(described_class.new('February', 2012).total_days).to eq(29) }
+    shared_examples_for 'total days' do |month, year, total_days|
+      it { expect(described_class.new(month, year).total_days).to eq(total_days) }
+    end
+    it_should_behave_like 'total days', 'January', 2014, 31
+    it_should_behave_like 'total days', 'February', 2014, 28
+    it_should_behave_like 'total days', 'March', 2014, 31
+    it_should_behave_like 'total days', 'April', 2014, 30
+    it_should_behave_like 'total days', 'May', 2014, 31
+    it_should_behave_like 'total days', 'June', 2014, 30
+    it_should_behave_like 'total days', 'July', 2014, 31
+    it_should_behave_like 'total days', 'August', 2014, 31
+    it_should_behave_like 'total days', 'September', 2014, 30
+    it_should_behave_like 'total days', 'October', 2014, 31
+    it_should_behave_like 'total days', 'November', 2014, 30
+    it_should_behave_like 'total days', 'December', 2014, 31
+    it_should_behave_like 'total days', 'February', 2012, 29
 
   end
 
   context '#weekends' do
-    it { expect(described_class.new('January', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('February', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('March', 2014).weekends).to eq(10) }
-    it { expect(described_class.new('April', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('May', 2014).weekends).to eq(9) }
-    it { expect(described_class.new('June', 2014).weekends).to eq(9) }
-    it { expect(described_class.new('July', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('August', 2014).weekends).to eq(10) }
-    it { expect(described_class.new('September', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('October', 2014).weekends).to eq(8) }
-    it { expect(described_class.new('November', 2014).weekends).to eq(10) }
-    it { expect(described_class.new('December', 2014).weekends).to eq(8) }
+    shared_examples_for 'weekends' do |month, year, weekends|
+      it { expect(described_class.new(month, year).weekends).to eq(weekends) }
+    end
+    it_should_behave_like 'weekends', 'January', 2014, 8
+    it_should_behave_like 'weekends', 'February', 2014, 8
+    it_should_behave_like 'weekends', 'March', 2014, 10
+    it_should_behave_like 'weekends', 'April', 2014, 8
+    it_should_behave_like 'weekends', 'May', 2014, 9
+    it_should_behave_like 'weekends', 'June', 2014, 9
+    it_should_behave_like 'weekends', 'July', 2014, 8
+    it_should_behave_like 'weekends', 'August', 2014, 10
+    it_should_behave_like 'weekends', 'September', 2014, 8
+    it_should_behave_like 'weekends', 'October', 2014, 8
+    it_should_behave_like 'weekends', 'November', 2014, 10
+    it_should_behave_like 'weekends', 'December', 2014, 8
 
   end
 
